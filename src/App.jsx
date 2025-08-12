ff38984ad33fc4c0e9951f7bc17dcbb75633cdff
buraq falah <buraqfalah1@gmail.com>
2025-08-12 19:13:42 +0800

    setup project requierments 1-install reactjs by vite 2-install tailwind & daisy plugin

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="text-red-600 text-2xl font-bold p-4 text-center">
+      {/* Your content goes here */}
+      Hello from the eCommerce App!
+      <p>Welcome to our online store.</p>
+    </div>
+  );
+};
+
+export default App;
