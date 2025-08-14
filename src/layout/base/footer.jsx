e5204e7de36fae96b5af36509072841920422272
buraq falah <buraqfalah1@gmail.com>
2025-08-14 12:21:47 +0800

    create footerData json file inside static folder

--- src/layout/base/footer.jsx
+++ src/layout/base/footer.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import FooterData from "../../static/footer_data.json";
 
 const Footer = () => {
-    return (
-        <footer className='text-center mt-5 py-3 bg-red-600 text-white text-lg '>
-            <p>&copy; {new Date().getFullYear()} Ecommerce. All rights reserved.</p>
-        </footer>
-    );
+  return (
+    <footer class="bg-white rounded-lg dark:bg-gray-900 m-4">
+      <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © {new Date().getFullYear()}{" "} 
+          <a href={`${FooterData.url}`} className="hover:underline">
+            {FooterData.website}™
+          </a>
+          . {FooterData.copy_right}.
+        </span>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
